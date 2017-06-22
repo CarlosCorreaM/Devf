b73917daee002be47c3d398b5f97aca872721297
Carlos Correa Mandujano <carloscorrea.dev@gmail.com>
2017-06-21 21:58:01 -0500

    día 3 Jquery

---
+++ dia3/Devf/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+     $('.next').click(function() {
+       $('#logo1').fadeOut();
+       $('#product').fadeOut();
+       $('#tech').fadeIn();
+       $('#madein').fadeIn();
+       $('.contain').fadeIn();
+   });
+});
